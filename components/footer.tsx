a5a1110e42488df295470c72be2962c3f2f89d22
Norah <norah.haque@gmail.com>
2025-12-23 17:01:09 -0600

    edited footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,9 +6,6 @@ export default function Footer() {
       <small className="mb-1.5 block text-[0.65rem]">
         &copy; 2025 Norah Haque. All rights reserved.
       </small>
-      <p className="text-[0.65rem] leading-tight">
-        <span className="font-semibold">About this website:</span> Powered by React, Next.js, TypeScript, Tailwind, Framer Motion, and Vercel.
-      </p>
     </footer>
   );
 }
